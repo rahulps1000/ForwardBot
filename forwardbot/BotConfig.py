b1a89bd6e6e77e81d088a1065dfc3ed8b2a052b1
subinps <64341611+subinps@users.noreply.github.com>
2021-02-22 00:18:37 +0530

    Fix

--- forwardbot/BotConfig.py
+++ forwardbot/BotConfig.py
@@ -2,7 +2,7 @@ from os import environ
 class Config(object):
     API_ID = environ.get("API_ID", None)
     API_HASH = environ.get("API_HASH", None)
-    BOT_TOKEN = environ.get("TOKEN", None)
+    BOT_TOKEN = environ.get("BOT_TOKEN", None)
     STRING_SESSION = environ.get("STRING", None)
     SUDO_USERS = environ.get("SUDO_USERS", None)
     COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "^/")
