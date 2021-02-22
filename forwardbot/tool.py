bb453e516ef2c1de4fe965cd3dde33c30502ad89
rahulps1000 <58558341+rahulps1000@users.noreply.github.com>
2021-02-22 21:42:01 +0530

    Create tool.py

---
+++ forwardbot/tool.py
@@ -0,0 +1,18 @@
+def media_type(message):
+    if message and message.photo:
+        return "Photo"
+    if message and message.audio:
+        return "Audio"
+    if message and message.voice:
+        return "Voice"
+    if message and message.video_note:
+        return "Round Video"
+    if message and message.gif:
+        return "Gif"
+    if message and message.sticker:
+        return "Sticker"
+    if message and message.video:
+        return "Video"
+    if message and message.document:
+        return "Document"
+    return None
